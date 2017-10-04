78af756d2579a4bd6c907700e95ac4e4a0775ccf
alexandre.legoff <alexandre.legoff@interalia.net>
2017-10-04 15:31:41 -0500

    en cosntruccion

--- js/src/02_main.js
+++ js/src/02_main.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
 	console.log("demo");
+
+	//$(".underConstruction ul li")
+
+	var activeLi = 0;
+
+	setInterval(function(){ 
+		changeLi();
+	}, 3000);
+
+	function changeLi(){
+		$(".underConstruction ul li").removeClass('active');
+		activeLi++;
+		if ( activeLi == 3) {
+			activeLi = 0;
+		}
+		$(".underConstruction ul li").eq(activeLi).addClass('active');
+	}
+
 });
