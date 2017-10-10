dd0e86ac57a12ce92e2d8938e495dcab8f01f233
alexandre.legoff <alexandre.legoff@interalia.net>
2017-10-10 16:46:49 -0500

    click

--- js/src/02_main.js
+++ js/src/02_main.js
@@ -1,5 +1,4 @@
 var ALPHA = ALPHA || {};
-
 ALPHA.lanch_modal = function( $elem ){
 	var html_modal = '<div class="modal">';
 			html_modal += '<div class="modal_container">';
@@ -8,43 +7,53 @@ ALPHA.lanch_modal = function( $elem ){
 		html_modal += '</div>';
 	$('body').append( html_modal );
 }
-
-
 $(document).ready(function(){
 	/*==========DEMO===========*/
-	var activeLi = 0;
-
-	setInterval(function(){ 
-		changeLi();
-	}, 3000);
-
-	function changeLi(){
-		$(".underConstruction ul li").removeClass('active');
-		activeLi++;
-		if ( activeLi == 3) {
-			activeLi = 0;
-		}
-		$(".underConstruction ul li").eq(activeLi).addClass('active');
-	}
+	var activeLi = 0;setInterval(function(){ changeLi();}, 3000);function changeLi(){$(".underConstruction ul li").removeClass('active');activeLi++;if ( activeLi == 3) {activeLi = 0;}$(".underConstruction ul li").eq(activeLi).addClass('active');}
 	/*========== END DEMO ===========*/
-
-	
+	/*==========Variables===========*/
 	var wHeight = $( window ).height();
 	var wWidth = $( window ).width();
-
+	var activeLiDemo = 0;
+	var activeLiNosotros = 0;
+	var activeLiContacto = 0;
+	var animationNosotros, animationContacto, animationServicios, animationClientes;
+	var item_width = $('.serviciosSection li').outerWidth(); 
+	var left_value = item_width * (-1); 
+	var item_height = $('.clientesSection li').outerHeight(); 
+	var top_value = item_height * (-1); 
+	var top_indent = parseInt($('.clientesSection ul').css('top')) - item_height;
+	var left_indent = parseInt($('.serviciosSection ul').css('left')) - item_width;
+	$('.serviciosSection li:first').before($('.serviciosSection li:last'));
+	$('.clientesSection li:first').before($('.clientesSection li:last'));
+	/*==========End Variables===========*/
+	/*==========Resize===========*/
 	$( window ).resize(function(){
-		wHeight = $( window ).height();
-		wWidth = $( window ).width();
 		resizeFunctions();
 	});
 
 	function resizeFunctions(){
+		wHeight = $( window ).height();
+		wWidth = $( window ).width();
 		$('section').height( (wHeight/2)+'px' );
-		$('section ul li').height( (wHeight/2)+'px' );
-		$('section ul li').width( (wWidth/2)+'px' );
+		liHeight = $('section').height();
+		liWidth = $('section').width();
+		$('section ul li').height( liHeight+'px' );
+		$('section ul li').width( liWidth+'px' );
+
+		item_width = $('.serviciosSection').outerWidth(); 
+		left_value = item_width * (-1); 
+		item_height = $('.clientesSection').outerHeight(); 
+		top_value = item_height * (-1); 
+		top_indent = parseInt($('.clientesSection ul').css('top')) - item_height;
+		left_indent = parseInt($('.serviciosSection ul').css('left')) - item_width;
+
+		$('.serviciosSection ul').css({'left' : left_value});
+		$('.clientesSection ul').css({'top' : top_value});
 	}
 	resizeFunctions();
-
+	/*==========End Resize===========*/
+	/*==========Click Logo===========*/
 	$('.logoImage').click(function($evt){
 		$evt.preventDefault();
 		var add_html = '<img src="images/gif_demo.gif" />'
@@ -55,58 +64,116 @@ $(document).ready(function(){
 			}, 1800
 		);
 	});
+	/*==========End Click Logo===========*/
+	/*==========Hover Animations===========*/
+	$('.nosotrosSection').mouseenter(function() {
+		animationNosotros = setInterval(function(){ changeLiNosotros(); }, 1500);
+	});
+	$('.contactoSection').mouseenter(function() {
+		animationContacto = setInterval(function(){ changeLiContacto(); }, 1500);
+	});
+	$('.serviciosSection').mouseenter(function() {
+		animationServicios = setInterval(function(){ changeLiServicios(); }, 1500);
+	});
+	$('.clientesSection').mouseenter(function() {
+		animationClientes = setInterval(function(){ changeLiClientes(); }, 1500);
+	});
 
-	/*==========DEMO List===========*/
-	var activeLiDemo = 0;
-
-
-	var item_width = $('.serviciosSection li').outerWidth(); 
-	var left_value = item_width * (-1); 
-	$('.serviciosSection li:first').before($('.serviciosSection li:last'));
-	$('.serviciosSection ul').css({'left' : left_value});
-
-
-
-
-	var item_height = $('.clientesSection li').outerHeight(); 
-	var top_value = item_height * (-1); 
-	$('.clientesSection li:first').before($('.clientesSection li:last'));
-	$('.clientesSection ul').css({'top' : top_value});
+	$('.nosotrosSection').mouseleave(function() {
+		clearTimeout(animationNosotros);
+	});
+	$('.contactoSection').mouseleave(function() {
+		clearTimeout(animationContacto);
+	});
+	$('.serviciosSection').mouseleave(function() {
+		clearTimeout(animationServicios);
+	});
+	$('.clientesSection').mouseleave(function() {
+		clearTimeout(animationClientes);
+	});
 	
-	/*$('.serviciosSection ul:not(:animated)').animate({'left' : left_indent}, 200, function () {
-			$('.serviciosSection li:last').after($('.serviciosSection li:first'));                 	
-			$('.serviciosSection ul').css({'left' : left_value});
-		});*/
-
-	setInterval(function(){ 
-		changeLiDemo();
-	}, 3000);
-
-	function changeLiDemo(){
+	function changeLiNosotros(){
 		$(".nosotrosSection ul li").removeClass('active');
-		$(".contactoSection ul li").removeClass('active');
-		activeLiDemo++;
-		if ( activeLiDemo == 4) {
-			activeLiDemo = 0;
+		activeLiNosotros++;
+		if ( activeLiNosotros == 4) {
+			activeLiNosotros = 0;
 		}
-		$(".nosotrosSection ul li").eq(activeLiDemo).addClass('active');
-		$(".contactoSection ul li").eq(activeLiDemo).addClass('active');
-
-
-
-		var left_indent = parseInt($('.serviciosSection ul').css('left')) - item_width;
+		$(".nosotrosSection ul li").eq(activeLiNosotros).addClass('active');
+	}
+	function changeLiContacto(){
+		$(".contactoSection ul li").removeClass('active');
+		activeLiContacto++;
+		if ( activeLiContacto == 4) {
+			activeLiContacto = 0;
+		}
+		$(".contactoSection ul li").eq(activeLiContacto).addClass('active');
+	}
+	function changeLiServicios(){
+		left_indent = parseInt($('.serviciosSection ul').css('left')) - item_width;
 		$('.serviciosSection ul:not(:animated)').animate({'left' : left_indent}, 500, function () {
 			$('.serviciosSection li:last').after($('.serviciosSection li:first'));                 	
 			$('.serviciosSection ul').css({'left' : left_value});
 		});
-
-
-		var top_indent = parseInt($('.clientesSection ul').css('top')) - item_height;
+	}
+	function changeLiClientes(){
+		top_indent = parseInt($('.clientesSection ul').css('top')) - item_height;
 		$('.clientesSection ul:not(:animated)').animate({'top' : top_indent}, 500, function () {
-			$('.clientesSection li:last').after($('.clientesSection li:first'));                 	
+			$('.clientesSection li:last').after($('.clientesSection li:first'));
 			$('.clientesSection ul').css({'top' : top_value});
 		});
 	}
+	/*========== End Hover Animations ===========*/
 
-	/*========== END DEMO List ===========*/
+	$('.nosotrosSection').click(function($evt){
+		$evt.preventDefault();
+		$(this).addClass('openSection');
+		$('.contactoSection , .serviciosSection, .clientesSection').animate(
+			{ 'width' : '0', 'height' : '0', 'opacity' : '0' }, 
+			250, function(){
+				$('.nosotrosSection').animate(
+					{ 'width' : '100%', 'height' : '100%' }, 
+					250
+				);
+			}
+		);
+	});
+	$('.contactoSection').click(function($evt){
+		$evt.preventDefault();
+		$(this).addClass('openSection');
+		$('.nosotrosSection , .serviciosSection, .clientesSection').animate(
+			{ 'width' : '0', 'height' : '0', 'opacity' : '0'  }, 
+			250, function(){
+				$('.contactoSection').animate(
+					{ 'width' : '100%', 'height' : '100%' }, 
+					250
+				);
+			}
+		);
+	});
+	$('.serviciosSection').click(function($evt){
+		$evt.preventDefault();
+		$(this).addClass('openSection');
+		$('.contactoSection , .nosotrosSection, .clientesSection').animate(
+			{ 'width' : '0', 'height' : '0', 'opacity' : '0'  }, 
+			250, function(){
+				$('.serviciosSection').animate(
+					{ 'width' : '100%', 'height' : '100%' }, 
+					250
+				);
+			}
+		);
+	});
+	$('.clientesSection').click(function($evt){
+		$evt.preventDefault();
+		$(this).addClass('openSection');
+		$('.contactoSection , .serviciosSection, .nosotrosSection').animate(
+			{ 'width' : '0', 'height' : '0', 'opacity' : '0'  }, 
+			250, function(){
+				$('.clientesSection').animate(
+					{ 'width' : '100%', 'height' : '100%' }, 
+					250
+				);
+			}
+		);
+	});
 });
